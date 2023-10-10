935c8edb7909227f383a08b7f589dd109e4d5d8e
M-Oduvanchick <147337778+M-Oduvanchick@users.noreply.github.com>
2023-10-10 21:46:16 +0500

    Proba.py

--- Proba.py
+++ Proba.py
@@ -1,5 +1,2 @@
-
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 91, 108, 111]
-for elem in a:
-    if elem > 56:
-        print(elem)
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+print(list(filter(lambda elem: elem <5, a)))
