f341c8e427988dec5451a48f0430247e1726e058
M-Oduvanchick <147337778+M-Oduvanchick@users.noreply.github.com>
2023-12-12 21:06:38 +0500

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	setlocale(LC_ALL, "Russian");
+
+	cout << "¬ведите длину основани€: ";
+	cin >> n;
+	cout << endl;
+	if (n % 2 == 0)
+	{
+		cout << "ќшибка. ¬ведите нечетное число: ";
+		cin >> n;
+		cout << endl;
+	}
+	for (int i = 1; i <= n / 2 + 1; i++)
+	{
+		for (int j = 1; j <= (n + 1) / 2 - i; j++)
+		{
+			cout << ' ';
+		}
+		for (int j = 1; j <= i * 2 - 1; j++)
+		{
+			cout << '*';
+		}
+		cout << endl;
+	}
+	return 0;
+}
